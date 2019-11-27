df4b5e5c54473a63011f73742e8f3dbd5f90e143
Remmelt Pit <rpit@ebay.com>
2019-11-27 23:14:34 +0100

    autoWIP

--- evelina/push_events.go
+++ evelina/push_events.go
@@ -9,6 +9,7 @@ func HandlePush(payload github.PushEvent, l *log.Entry) error {
 	l = l.WithFields(log.Fields{
 		"ref":     *payload.Ref,
 		"repoURL": *payload.Repo.URL,
+		"head":    *payload.Head,
 	})
 	l.Info("Handling push")
 
@@ -16,7 +17,16 @@ func HandlePush(payload github.PushEvent, l *log.Entry) error {
 		l.Info("Not triggering, not pushed to master")
 		return nil
 	}
+
+	return packageApp(payload, l)
+}
+
+func packageApp(payload github.PushEvent, l *log.Entry) error {
 	l.Info("Trigger a package run, creating a new release")
+
+	if *payload.Repo.URL == "evelina_test"{
+		nomad.
+	}
 
 	return nil
 }
